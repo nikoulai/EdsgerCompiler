2694cccb64e1260a92542ab5a099a22ba30032c3
Aspa Skalidi <aspa.sk7@gmail.com>
2019-03-13 13:29:49 +0200

    Test inputs

---
+++ tests/tests/stdio.h
@@ -0,0 +1,11 @@
+void writeInteger (int n);
+void writeBoolean (bool b);
+void writeChar (char c);
+void writeReal (double d);
+void writeString (char *s);
+
+int readInteger();
+bool readBoolean();
+char readChar();
+double readReal();
+void readString(int size, char *s);
