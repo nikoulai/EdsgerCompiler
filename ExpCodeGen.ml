0cd0e31a022973b5be6fb59944d9b40216620218
Nikos <nikoulai94@hotmail.com>
2019-03-26 00:18:33 +0200

    Hygiene

--- ExpCodeGen.ml
+++ ExpCodeGen.ml
@@ -48,22 +48,14 @@ let contains s1 s2 =
   with Not_found -> false
 
 
-(*Environment Interface*)
+
 let rec update_env_without_hashtbl name env =
-  (* let name_env = String.concat  "_" [name;"env"] in *)
-  (* let name_to_env = match env with Global(_) -> name | _ -> name_env in *)
-  (* let _ = Hashtbl.add named_values name_to_env llval in  *)(*just to get it's type??*)
   match env with
   | Global (names) -> Global(name::names)
   | Nested (names,e) -> Nested (name::names,e)
 
 and clear_env env_list =
-  (* match env with *)
-  (* |Global([])  *)
-  (*  |Nested([],_) -> () *)
-  (* |Global(h::t) -> Hashtbl.remove named_values h; clear_env (Global(t)) *)
-  (* |Nested(h::t,a)->Hashtbl.remove named_values h; clear_env (Nested(t,a)) *)
-  List.iter (Hashtbl.remove named_values) env_list
+    List.iter (Hashtbl.remove named_values) env_list
 
 and get_env_of_called env args params =
   let argscnt = List.length args in
@@ -80,7 +72,7 @@ and get_env_of_called env args params =
   in walk env
 
 and update_env name env=
-  let name_env = name in(* String.concat  "_" [name;"env"] in *)
+  let name_env = name in
   let name_to_env = match env with Global(_) -> name | _ -> name_env in
   match env with
   | Global (names) -> Global(name_to_env::names)
@@ -95,7 +87,7 @@ and update_env_with_params params en =
 and env_to_set env =
   let rec walk env acc =
     match env with
-    | Global (names) -> let set_to_add = SS.of_list names in SS.union set_to_add acc (*a global value may be shadowed*)
+    | Global (names) -> let set_to_add = SS.of_list names in SS.union set_to_add acc
     | Nested (names,env) -> let set_to_add = SS.of_list names in
 
                             let new_acc = SS.union set_to_add acc in
@@ -112,7 +104,7 @@ and get_env_params_types env global_decs =
   let has_name_in_dec name dec =
     match dec with
     |Ast.Decl(n,_) -> (name = n)
-    (* |Ast.Complex_declarator (n,_) -> (name = n) *)
+
   in
   let has_name_in_var_dec name element =
     match element with
@@ -122,15 +114,13 @@ and get_env_params_types env global_decs =
 
   in
   let find_type_from_global name gl =
-    (* Printf.printf "Searching for %s\n" name; *)
-    (* Printf.printf "Global length: %d\n" (List.length gl); *)
-    let dec = List.find (has_name_in_var_dec name) gl (* with Not_found -> Ast.Variable_dec(TYPE_none,[]) *)
+    let dec = List.find (has_name_in_var_dec name) gl
     in match dec with
        | Ast.Vardecl(ty,l) -> (let wanted = List.find (has_name_in_dec name) l in
                                     let ty = findLltype ty in
                                     match wanted with
                                     |Ast.Decl _ -> pointer_type ty
-                                    (* |Ast.Complex_declarator _ -> pointer_type (pointer_type ty) *)
+
                                     )
        | _ -> Printf.printf "unexpected type"; int_type
   in
@@ -163,7 +153,6 @@ and env_to_id_list env =
   let env_list = env_to_list env in
   List.map (fun x -> Eid(x)) env_list
 
-(*Probably we don't need this*)
 and get_args_diff_env args env=
   let args_set = SS.of_list args in
   let env_set = env_to_set env in
@@ -176,10 +165,6 @@ and get_param_names params =
     | Ast.Param(_,name) -> name
     | Ast.ParamByRef(_,name) -> name
   in List.map get_param_name params
-
-
-(*End of environment interface*)
-
 
 let rec print_list = function
     [] -> ()
@@ -200,39 +185,27 @@ let rec find_function fun_name fun_name_list =
 let rec code_gen_exp exp =
   match exp with
     Eid name ->  let v =
-                  (* Printf.printf "Trying to find %s...\n" name; *)
-                  (* print_hashtbl named_values; *)
                   try
                     Hashtbl.find named_values name
-                                 (* with Not_found -> raise (Type_error "Variable not found!") in *)
                   with Not_found -> raise (Type_error "Variable not found") in
                 v
   | Eint int -> let value = const_int int_type int in
-               (* let llvalue = build_store value "inttmp" builder in *) (* dump_value value; *)
                value;
   | Edoub double -> const_float double_type double
-  | Echar char -> const_int char_type (code char)(* const_string context (escaped char) *)
-  (* | String "NULL" -> const_pointer_null (pointer_type int_type) *)
+  | Echar char -> const_int char_type (code char)
   | Estr str -> build_global_stringptr str "strtmp" builder
   | Ebool bool -> const_int bool_type (bool_to_int bool)
-  (* | Constant_exp exp -> code_gen_exp exp *)
   | Eapp (called_fun, args) ->
-   (* Look up the name in the module table. *)
    let create_param p = Param(Tnone,"dum") in
    let callee = find_function called_fun (!fun_names) in
    let params = params callee in
    let tmp = if(is_void callee) then "" else "calltmp" in
    let env_of_called = get_env_of_called !env args params in
    let env_args = env_to_id_list (env_of_called) in
-   (* let find_name_of_gl_decs decs = List.map (fun x -> match x with | Ast.Simple_declarator(name)-> name | Ast.Complex_declarator (name,_) ->  name) decs in *)
-   (* let names_global_pars = List.flatten (List.map (fun x-> match x with | Ast.Variable_dec(ty, decs) -> find_name_of_gl_decs decs) !global_decs)  in *)
-   (* let global_pars = List.map (fun x -> Id(x)) names_global_pars in *)
-   (* let args = args@global_pars@env_args_wo_global in *)
    let args = args@env_args in
    let args = Array.of_list args in
-   (* If argument mismatch error. *)
    let args = Array.mapi
-                ( fun i a -> let e = code_gen_exp args.(i) in (*used to be i*)
+                ( fun i a -> let e = code_gen_exp args.(i) in 
                              if(is_pointer args.(i)) then
                                (let typical_param = String.length(string_of_lltype (type_of(params.(i)))) in
                                 let actual_param = String.length( string_of_lltype ( type_of(e))) in
@@ -253,7 +226,6 @@ let rec code_gen_exp exp =
                                 else index in
                     let index = Array.of_list [index] in
                     let ir = build_gep act_array index "arraytmp" builder in
-                    (* dump_value ir; *)
                     ir
 | Eunop (e,op) -> (match op with
                       |  Tuamp->  code_gen_exp e
@@ -265,7 +237,7 @@ let rec code_gen_exp exp =
                                code_gen_exp expr
                       | Tupl -> code_gen_exp e
                       | Tunot -> let exp = code_gen_exp e in
-                               build_not exp "nottmp" builder (* Semantic !!!! *)
+                               build_not exp "nottmp" builder
                       | _ -> raise (Type_error "Wrong operator")
                      )
 
@@ -332,27 +304,21 @@ let rec code_gen_exp exp =
                                | Tbor -> let ir1_i1 = build_trunc_or_bitcast ir1 (i1_type context) "first_cast" builder in
                                          let ir2_i1 = build_trunc_or_bitcast ir2 (i1_type context)"second_cast" builder in
                                          build_or ir1_i1 ir2_i1 "ortmp" builder
-                               | Tbcom -> (* dump_value ir1; dump_value ir2;*)
-                                  ir2 (* this is probably wrong *)
+                               | Tbcom ->
+                                  ir2
                                | _ -> (Error.error "%s: Unkown binary operator while producing IR" ("") ;const_null int_type)
                               )
                             )
 
 
-|Eunas (e,op) -> (*Only Id's are acceptable*) (* print_string "vrike prefix unary reee"; *)
+|Eunas (e,op) ->
   let ir = code_gen_exp e in
-  (* dump_type (type_of ir); *)
   (match op with
    | Tppl ->
-      (* dump_value ir; *)
-      (* print_string("before search"); *)
       let expr = if (is_double_pointer ir) then Ebop(e, Edoub 1.0,Tbpl)
-                                                         (* else if (is_op_with_pointer ir) then (print_string"vrike pointer"; Array(e,Int 1)) *)
                  else Ebop(e,Eint 1,Tbpl) in
       let exp = code_gen_exp expr in
       let _ = build_store exp ir builder in
-      (* dump_value ir; *)
-      (* print_string "telos prefix"; *)
       exp
    | Tmmin ->
       let expr = if (is_double_pointer ir) then Ebop(e, Edoub 1.0,Tbpl)
@@ -369,7 +335,6 @@ let rec code_gen_exp exp =
    | Tppl -> let expr = if (is_double_pointer ir) then Ebop(e, Edoub 1.0,Tbpl)
                         else Ebop(e,Eint 1,Tbpl) in
              let exp = code_gen_exp expr in
-             (* dump_value exp; *)
              let _ = build_store exp ir builder in
              let sube = if (is_double_pointer exp) then Ebop(e, Edoub 1.0,Tbmin)
                         else Ebop(e,Eint 1,Tbmin) in
@@ -387,30 +352,22 @@ let rec code_gen_exp exp =
    | _ -> Error.error "%s: Don't know what to do with prefix operator:" ""; const_null int_type
   )
 
-|Ebas (e1,e2, op) -> (*Postfix and Prefix unary ass have the same result as long as there is no other assignment in the statement...*)
-  (*WARNING: We may assign pointer values not actual values!!!*)
-  (* let _ = match e1 with *)
-  (*   | Id name -> Printf.printf "%s\n" name *)
-  (*   | _ -> Printf.printf "Too complex\n" *)
-  (* in *)
+|Ebas (e1,e2, op) ->
   (match op with
    | Tba ->
-      (* Printf.printf ("Simple assignment\n"); *)
 
       (match e2 with
        | Eunas1 (e, op) ->
-          (* print_string "postfix operator"; *)
           let rhs' = code_gen_exp e in
-          let rhs = if(is_pointer e) then((* print_string "it is"; *) build_load rhs' "loadtmp" builder)
+          let rhs = if(is_pointer e) then( build_load rhs' "loadtmp" builder)
                     else rhs' in
           let lhs = code_gen_exp e1 in
           let _ = build_store rhs lhs builder in
-          (* dump_type (type_of rhs'); *)
           let oper =( match op with
                       | Tppl -> Tbpl
                       | _ -> Tbmin )in
           let adde = if(is_double_pointer rhs') then( Ebop(e,Edoub 1.0,oper))
-                     else ( (* print_string "kati ginetai"; *) Ebop(e,Eint 1, oper)) in
+                     else (  Ebop(e,Eint 1, oper)) in
           let add = code_gen_exp adde in
 
 
@@ -418,8 +375,7 @@ let rec code_gen_exp exp =
           lhs
        | Eunas(op, e) ->
           let ret_val = code_gen_exp e2 in
-          let rhs = (* if (is_op_with_pointer ret_val) then ret_val *) ret_val in
-          (* dump_value ret_val; *)
+          let rhs =  ret_val in
           let lhs = code_gen_exp e1 in
           let _ = build_store rhs lhs builder in
           lhs
@@ -436,13 +392,10 @@ let rec code_gen_exp exp =
             else rhs
           in
           let lhs = code_gen_exp e1 in
-          (* dump_value lhs; *)
           let _ = build_store rhs lhs builder in
-          (* dump_value res; *)
           lhs
       )
    | _ ->
-      (* Printf.printf ("Complex asssignment\n"); *)
       let op = (match op with
                 | Tbapl -> Tbpl
                 | Tbamin -> Tbmin
@@ -463,20 +416,18 @@ let rec code_gen_exp exp =
                         (ignore(String.index ty_exp '*');
                          build_load exp "loadcastingtmp" builder)
                       with Not_found -> ( exp) in
-                    (* print_string ty_exp; *)
                     if (contains ty_exp "x86_fp80") then (
-                      (* print_string ("found double"); *)
                       let ty = findLltype t1 in
                       build_fptosi exp ty "castingtmp" builder)
                     else if (contains ty_exp  "i16") then
-                      ( (* print_string ("found int"); *)
+                      (
                         match t1 with
-                        | Tdouble -> let ty = findLltype t1 in  (* if we want double cast *)
+                        | Tdouble -> let ty = findLltype t1 in
                                          build_sitofp exp ty "castingtmp" builder
                         | _ -> let ty = findLltype t1 in
                                build_trunc_or_bitcast exp ty "castingtmp" builder )
                     else if (contains ty_exp "i8") then
-                      ((* print_string ("found char"); *)
+                      (
                        match t1 with
                        | Tint -> let ty = findLltype t1 in
                                      build_sext_or_bitcast exp ty "castingtmp" builder
@@ -485,7 +436,7 @@ let rec code_gen_exp exp =
                        | _ -> let ty = findLltype t1 in
                               build_trunc_or_bitcast exp ty "castingtmp" builder )
                     else
-                      ((* print_string ("mpike sto else"); *)
+                      (
                        match t1 with
                        | Tdouble -> let ty = findLltype t1 in
                                         build_sitofp exp ty "castingtmp" builder
@@ -498,10 +449,9 @@ let rec code_gen_exp exp =
                          let cond_val = if (is_pointer e1) then build_load condition "loadcon" builder
                                         else condition in
                          let cond_val = build_icmp Icmp.Ne cond_val zero "ifcond" builder in
-                         (* let cond_val = condition (\* build_fcmp Fcmp.One condition zero "ifcond" builder *\) in *)
-                         let start_bb = insertion_block builder in (* start_bb contains the basic block *)
+                         let start_bb = insertion_block builder in
                          let the_function = block_parent start_bb in
-                         let then_bb = append_block context "then" the_function in (* creates the then block *)
+                         let then_bb = append_block context "then" the_function in
                          position_at_end then_bb builder;
 
                          let then_val =  code_gen_exp e2 in
@@ -521,12 +471,11 @@ let rec code_gen_exp exp =
   ignore (build_cond_br cond_val then_bb else_bb builder);
   position_at_end new_then_bb builder; ignore (build_br merge_bb builder);
   position_at_end new_else_bb builder; ignore (build_br merge_bb builder);
-  (* Finally, set the builder to the end of the merge block. *)
   position_at_end merge_bb builder;
   phi
 
 
-|Enew (t, None) -> let ty = findLltype t in (*find types*)
+|Enew (t, None) -> let ty = findLltype t in
                      build_malloc (ty) "malloctmp" builder
 
 |Enew (t, Some e) -> let ty = findLltype t in
@@ -636,10 +585,10 @@ and  ltype_of_type = function
         | Tint ->  i32_type context
         | Tbool -> i1_type context
         | Tchar -> i8_type context
-        | Tdouble ->x86fp80_type context (* double_type context *)
+        | Tdouble ->x86fp80_type context
         | Tvoid -> void_type context
         | Tptr t -> pointer_type (ltype_of_type t)
-        | Tarray (a,b) ->(*pointer_type (ltype_of_type a) *)array_type (ltype_of_type a) b
+        | Tarray (a,b) ->array_type (ltype_of_type a) b
         | Tnone -> ltype_of_type Tvoid
 
 
